9b9297e02819d2840adab7c3b0f0d31e98576de7
jsponenberg <Berg@Jeffreys-MacBook-Pro.local>
2019-05-23 03:19:52 -0400

    added previous day's value

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"net/http"
 )
 
-//JSONData holds the api data
-type JSONData struct {
+//CurrentPrice holds the api data
+type CurrentPrice struct {
 	BPI struct {
 		USD struct {
 			Code string `json:"code"`
@@ -17,6 +17,13 @@ type JSONData struct {
 		} `json:"USD"`
 	} `json:"bpi"`
 }
+
+//Historical holds the api data
+//type Historical struct {
+//	BPI struct {
+//		Previous interface{}
+//	} `json:"bpi"`
+//}
 
 func main() {
 	response, err := http.Get("https://api.coindesk.com/v1/bpi/currentprice.json")
@@ -32,8 +39,30 @@ func main() {
 			log.Fatal(err)
 		}
 
-		btcData := JSONData{}
+		btcData := CurrentPrice{}
 		err = json.Unmarshal(btcPrice, &btcData)
 		fmt.Printf("The current price of bitcoin is $%s.\n", btcData.BPI.USD.Rate)
+	}
+
+	res, err := http.Get("https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK {
+		histPrice, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var histData map[string]interface{}
+		err = json.Unmarshal(histPrice, &histData)
+
+		previous := histData["bpi"].(map[string]interface{})
+		for _, value := range previous {
+			fmt.Printf("Yesterday's price of bitcoin was $%f.\n", value.(float64))
+		}
 	}
 }
